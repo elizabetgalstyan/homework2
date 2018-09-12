ff0e14c5c25cca619185d2b699e66bdaf0b23236
unknown <elizabet_galstyan@edu.aua.am>
2018-09-12 12:45:20 +0400

    Homework2

---
+++ homework2.js
@@ -0,0 +1,18 @@
+const num1=10;
+const num2=5;
+const num3=num1*num2;
+console.log(num1+num3);
+
+const grade=70;
+if(grae>60) {
+console.log("Pass")
+};
+
+
+const age=18;
+if(age<18) {
+console.log("Not permitted")
+}; 
+else {
+console.log("Welcome")
+};
